ad2cfafb5fe22727f71278ce2908fadf3df3c15f
niftylettuce <niftylettuce@gmail.com>
2018-07-30 03:51:38 -0500

    Fixed since V4 addLevel method did not actually work

--- logger.js
+++ logger.js
@@ -3,7 +3,9 @@ const consola = require('consola');
 const pino = require('pino')();
 
 // <https://github.com/pinojs/pino/issues/460>
-pino.addLevel('log', 30);
+// https://github.com/pinojs/pino/issues/460#issuecomment-408793207
+// pino.addLevel('log', 30);
+pino.log = pino.info;
 
 const logger = new Axe({
   logger: process.env.NODE_ENV === 'production' ? pino : consola,
